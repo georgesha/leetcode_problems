4a5484f077dc3eab6ed725738e770290c0b9fdd5
zsha2 <shazjgeorge@gmail.com>
2018-11-04 18:08:34 -0500

    Updated 2018/11/04

---
+++ leetcode_219.java
@@ -0,0 +1,17 @@
+class Solution {
+  public boolean containsNearbyDuplicate(int[] nums, int k) {
+    if(nums.length == 0 || k == 0) {
+      return false;
+    }
+    HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+    for(int i = 0; i < nums.length; i++) {
+      if(map.containsKey(nums[i]) && i - map.get(nums[i]) <= k) {
+        return true;
+      }
+      else {
+        map.put(nums[i], i);
+      }
+    }
+    return false;
+  }
+}
