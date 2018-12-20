0af114f3b5f1ecaa337b3c20591d08b38b0a3d89
zsha2 <shazjgeorge@gmail.com>
2018-12-20 17:12:18 -0500

    Updated 2018/12/20

---
+++ leetcode_59.java
@@ -0,0 +1,36 @@
+class Solution {
+  public int[][] generateMatrix(int n) {
+    int[][] result = new int[n][n];
+    int left = 0, right = result[0].length - 1, top = 0, bottom = result.length - 1, num = 1;
+    while (left <= right && top <= bottom) {
+      for (int i = left; i <= right; i++) {
+        result[top][i] = num;
+        num++;
+      }
+      top++;
+
+      for (int i = top; i <= bottom; i++) {
+        result[i][right] = num;
+        num++;
+      }
+      right--;
+
+      if (top <= bottom) {
+        for (int i = right; i >= left; i--) {
+          result[bottom][i] = num;
+          num++;
+        }
+      }
+      bottom--;
+
+      if (left <= right) {
+        for (int i = bottom; i >= top; i--) {
+          result[i][left] = num;
+          num++;
+        }
+      }
+      left++;
+    }
+    return result;
+  }
+}
