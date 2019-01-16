c79e16f9f2c0f3d9d40cca83024f0fe971d9e521
zsha2 <shazjgeorge@gmail.com>
2019-01-15 20:08:38 -0500

    Updated 2019/01/15

---
+++ leetcode_19.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+  public ListNode removeNthFromEnd(ListNode head, int n) {
+    ListNode temp = new ListNode(0);
+    temp.next = head;
+    ListNode slow = temp, fast = temp;
+    for (int i = 0; i <= n; i++) {
+      fast = fast.next;
+    }
+    while (fast != null) {
+      slow = slow.next;
+      fast = fast.next;
+    }
+    slow.next = slow.next.next;
+    return temp.next;
+  }
+}
